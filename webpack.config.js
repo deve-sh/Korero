012bebdc47152565bc79cf0c6e701e9c495a2dde
Devesh Kumar <devesh2027@gmail.com>
2023-02-03 21:18:57 +0530

    feat: Configured compilation using webpack instead of typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,34 @@
 const path = require("path");
 
 module.exports = {
-	entry: "./dist/index.js",
+	entry: "./src/index.ts",
 	output: {
 		path: path.resolve(__dirname, "dist"),
-		filename: "index.js",
+		filename: "korero.js",
 		libraryExport: "default",
 		library: "korero",
 		libraryTarget: "umd",
 		globalObject: "this",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(tsx|ts)$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: [
+							"@babel/preset-env",
+							["@babel/preset-react", { runtime: "automatic" }],
+						],
+					},
+				},
+			},
+			{ test: /\.(tsx|ts)?$/, use: "ts-loader" },
+		],
+	},
+	resolve: {
+		extensions: ["", ".ts", ".tsx"],
 	},
 };
