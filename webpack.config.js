ef57fb1ff0b0d1b1e35f7a8369aaaba7d52f3f34
Devesh Kumar <devesh2027@gmail.com>
2023-02-04 16:53:24 +0530

    Reverted webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,13 @@
 const path = require("path");
 
 module.exports = {
-	entry: "./src/index.ts",
+	entry: "./dist/index.js",
 	output: {
 		path: path.resolve(__dirname, "dist"),
-		filename: "korero.js",
+		filename: "index.js",
 		libraryExport: "default",
 		library: "korero",
 		libraryTarget: "umd",
 		globalObject: "this",
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(tsx|ts)$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: "babel-loader",
-					options: {
-						presets: [
-							"@babel/preset-env",
-							["@babel/preset-react", { runtime: "automatic" }],
-						],
-					},
-				},
-			},
-			{ test: /\.(tsx|ts)?$/, use: "ts-loader" },
-		],
-	},
-	resolve: {
-		extensions: ["", ".ts", ".tsx"],
 	},
 };
