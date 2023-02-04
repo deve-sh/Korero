80ab9d3d504d9aa5ca37372b1848777c0a068ae5
Devesh Kumar <devesh2027@gmail.com>
2023-02-04 16:58:20 +0530

    Valid User check at useOnAuthStateChange level

--- src/API/auth.ts
+++ src/API/auth.ts
@@ -6,10 +6,7 @@ import {
 } from "firebase/auth";
 
 import type SupportedAuthMethods from "../types/SupportedAuthTypes";
-import type User from "../types/User";
 import getAuth from "../firebase/authentication";
-
-import configStore from "../config";
 
 export const signOut = async () => {
 	try {
@@ -29,18 +26,6 @@ export const signIn = async (method: SupportedAuthMethods) => {
 
 		if (!data || !data.user)
 			throw new Error("Something went wrong with user sign in.");
-
-		// Check for user authenticity
-		const { isUserAllowed } = configStore.get();
-		const shouldCheckForUserAuthenticity = typeof isUserAllowed === "function";
-		if (shouldCheckForUserAuthenticity) {
-			const signedInUser = data.user as User;
-			const isValidUser = await isUserAllowed(signedInUser);
-			if (isValidUser) return { data };
-
-			await signOut();
-			throw new Error("You are not allowed to use this feature.");
-		}
 
 		return { data };
 	} catch (error) {
--- src/ui/Globals/useOnAuthStateChange.ts
+++ src/ui/Globals/useOnAuthStateChange.ts
@@ -1,17 +1,29 @@
 import { useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 
-import getAuth from "../../firebase/authentication";
+import configStore from "../../config";
 
 import type User from "../../types/User";
+import getAuth from "../../firebase/authentication";
 import useAuth from "../state/auth";
+import { signOut } from "../../API/auth";
 
 const useOnAuthStateChange = () => {
 	const [user, setUser] = useAuth();
 
 	useEffect(() => {
-		const unsubscribe = onAuthStateChanged(getAuth(), (user) => {
+		const unsubscribe = onAuthStateChanged(getAuth(), async (user) => {
 			if (!user) return setUser(null);
+
+			// Check for user authenticity
+			const { isUserAllowed } = configStore.get();
+			const shouldCheckForUserAuthenticity =
+				typeof isUserAllowed === "function";
+			if (shouldCheckForUserAuthenticity) {
+				const signedInUser = user as User;
+				const isValidUser = await isUserAllowed(signedInUser);
+				if (!isValidUser) await signOut();
+			}
 
 			const userObj = {
 				uid: user.email,
