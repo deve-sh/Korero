4b8bf191def4527a554c54bc54487d8ed59bfccd
Devesh Kumar <devesh2027@gmail.com>
2023-02-11 22:16:30 +0530

    feat: Added webpack build optimizations

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
 const path = require("path");
 
-module.exports = {
-	entry: "./dist/index.js",
-	output: {
-		path: path.resolve(__dirname, "dist"),
-		filename: "umd.js",
-		libraryExport: "default",
-		library: "korero",
-		libraryTarget: "umd",
-		globalObject: "this",
-	},
+module.exports = (mode) => {
+	const config = {
+		entry: "./dist/index.js",
+		output: {
+			path: path.resolve(__dirname, "dist"),
+			filename: "umd.js",
+			libraryExport: "default",
+			library: "korero",
+			libraryTarget: "umd",
+			globalObject: "this",
+		},
+	};
+
+	if (!mode.production)
+		config.optimization = {
+			removeAvailableModules: false,
+			removeEmptyChunks: false,
+			splitChunks: false,
+		};
+
+	return config;
 };
