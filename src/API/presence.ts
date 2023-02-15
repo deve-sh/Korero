23b9ed33b1fb903861e0b4033e375932bb441ddb
Devesh Kumar <devesh2027@gmail.com>
2023-02-15 23:47:03 +0530

    Added random stale data removal timeout

--- src/API/presence.ts
+++ src/API/presence.ts
@@ -2,10 +2,13 @@ import {
 	collection,
 	doc,
 	setDoc,
+	updateDoc,
 	serverTimestamp,
-	type Timestamp,
-	type DocumentSnapshot,
 	deleteField,
+	runTransaction,
+	type DocumentSnapshot,
+	type FieldValue,
+	type Timestamp,
 } from "firebase/firestore";
 import md5 from "md5";
 
@@ -18,6 +21,16 @@ import type {
 } from "../types/PresenceStatus";
 import type User from "../types/User";
 import tabUniqueId from "../ui/Globals/generateUniqueIdForTab";
+
+const getURLHash = () => {
+	const url = window.location.origin + window.location.pathname;
+	const urlHash = md5(url);
+	return urlHash;
+};
+const collectionRef = collection(
+	getFirestore(),
+	PRESENCE_FIRESTORE_COLLECTION_NAME
+);
 
 export const processPresenceStatusDocumentsSnapshot = (
 	querySnapshot: DocumentSnapshot<PresenceStatusDocumentInFirestore>
@@ -38,25 +51,14 @@ export const processPresenceStatusDocumentsSnapshot = (
 };
 
 export const getPresenceStatusesForPageDocRef = () => {
-	const url = window.location.origin + window.location.pathname;
-	const urlHash = md5(url);
-	const collectionRef = collection(
-		getFirestore(),
-		PRESENCE_FIRESTORE_COLLECTION_NAME
-	);
+	const urlHash = getURLHash();
 	const docRef = doc(collectionRef, urlHash);
 	return docRef;
 };
 
 export const setPresenceStatus = async (user: User, status: PresenceStatus) => {
 	try {
-		const url = window.location.origin + window.location.pathname;
-		const urlHash = md5(url);
-		const collectionRef = collection(
-			getFirestore(),
-			PRESENCE_FIRESTORE_COLLECTION_NAME
-		);
-
+		const urlHash = getURLHash();
 		const docRef = doc(collectionRef, urlHash);
 		const updates = {
 			[user.uid + "-" + tabUniqueId]: {
@@ -75,18 +77,50 @@ export const setPresenceStatus = async (user: User, status: PresenceStatus) => {
 
 export const removePresenceStatus = async (uid: string) => {
 	try {
-		const url = window.location.origin + window.location.pathname;
-		const urlHash = md5(url);
-
-		const collectionRef = collection(
-			getFirestore(),
-			PRESENCE_FIRESTORE_COLLECTION_NAME
-		);
+		const urlHash = getURLHash();
 		const updates = { [uid + "-" + tabUniqueId]: deleteField() };
-
 		const docRef = doc(collectionRef, urlHash);
 		await setDoc(docRef, updates, { merge: true });
 		return {};
+	} catch (error) {
+		return { error };
+	}
+};
+
+export const deleteStaleEntriesInCurrentPagePresence = async () => {
+	try {
+		const urlHash = getURLHash();
+		const metadataDocRef = doc(
+			collectionRef,
+			urlHash,
+			"presencemetadata",
+			"metadatadoc"
+		); // Metadata subcollection
+		const presenceDocRef = doc(collectionRef, urlHash);
+		await runTransaction(getFirestore(), async (transaction) => {
+			// Running this as a transaction given transactions in Firestore will ensure that if the underlying document has changed.
+			// The computation happens again.
+			const metadataDoc = await transaction.get(metadataDocRef);
+			if (!metadataDoc.data() || metadataDoc.data()?.beingUpdated)
+				// Safe to do, firestore's transactions will take care of any conflicts.
+				await updateDoc(metadataDocRef, { beingUpdated: true });
+
+			const presenceDoc = await transaction.get(presenceDocRef);
+			const presenceData =
+				presenceDoc.data() as PresenceStatusDocumentInFirestore;
+			const docUpdates: Record<string, FieldValue> = {};
+			for (let uid in presenceData) {
+				if (!presenceData.hasOwnProperty(uid)) continue;
+				const lastUpdatedAt = presenceData[uid].lastUpdatedAt as Timestamp;
+				// This is obviously not fool proof given the end user's date might be out of sync.
+				// But since this is an internal team collaboration tool, the chances for that happening are significantly low.
+				const oneHourBefore = new Date().getTime() - 60 * 60 * 1000;
+				if (lastUpdatedAt.toDate().getTime() < oneHourBefore)
+					docUpdates[uid] = deleteField();
+			}
+			transaction.update(presenceDocRef, docUpdates);
+		});
+		await updateDoc(metadataDocRef, { beingUpdated: false });
 	} catch (error) {
 		return { error };
 	}
--- src/ui/Globals/useMarkPresence.ts
+++ src/ui/Globals/useMarkPresence.ts
@@ -1,6 +1,10 @@
 import { useEffect } from "react";
 
-import { removePresenceStatus, setPresenceStatus } from "../../API/presence";
+import {
+	deleteStaleEntriesInCurrentPagePresence,
+	removePresenceStatus,
+	setPresenceStatus,
+} from "../../API/presence";
 
 import useAuth from "../state/auth";
 
@@ -22,11 +26,27 @@ const useMarkPresence = () => {
 			window.addEventListener("focus", onWindowFocus);
 			window.addEventListener("beforeunload", onTabClose);
 
+			// Every user connected to the system in real-time at some point will take ownership of clearing stale data for presence
+			// This can happen due to unclean exits from a page leading to onbeforeunload not firing and hence presence data being left from hours ago.
+			// The base for this is a random timeout so not a lot of end user devices do it all at once.
+			// And a Firestore transaction with a metadata document. A firestore transaction guarantees that a change to a document that's important to a transaction
+			// will be accounted for and hence the clients update the metadata document with a 'beingUpdated' field at the start of a transaction
+			// Any other client that might be making a transaction for the same purpose will get notified of the change and either stop or block any others from doing so.
+			const randomStaleDataRemovalTimeoutInterval =
+				60 * 1000 + Math.ceil(10 * 1000 * Math.random());
+			const randomStaleDataRemovalTimeout = setTimeout(
+				deleteStaleEntriesInCurrentPagePresence,
+				randomStaleDataRemovalTimeoutInterval
+			);
+
 			return () => {
 				removePresenceStatus(user.uid);
 				window.removeEventListener("beforeunload", onTabClose);
 				window.removeEventListener("focus", onWindowFocus);
 				document.removeEventListener("visibilitychange", onVisibilityChange);
+
+				if (randomStaleDataRemovalTimeout)
+					clearTimeout(randomStaleDataRemovalTimeout);
 			};
 		}
 	}, [user?.uid]);
