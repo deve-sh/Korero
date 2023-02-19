390995725a9e17e119365f684d1ef54f23ab680d
Devesh Kumar <devesh2027@gmail.com>
2023-02-19 22:34:54 +0530

    Added util unction to get sliced content from DOM Selection

---
+++ src/utils/selections/getContentFromDOMSelection.ts
@@ -0,0 +1,14 @@
+const getContentFromDOMSelection = () => {
+	const selection = window.getSelection();
+	if (!selection || !selection.rangeCount) return null;
+
+	const range = selection.getRangeAt(0);
+	if (!range) return null;
+
+	const content = selection.toString();
+	if (content.length > 100) return content.slice(0, 100) + "...";
+
+	return content;
+};
+
+export default getContentFromDOMSelection;
