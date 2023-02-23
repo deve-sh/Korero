284284320af25d5c3cad3333ca2b3105b97dfbb1
Devesh Kumar <devesh2027@gmail.com>
2023-02-23 18:17:16 +0530

    Tests: Disabled Playwright parallelism + Added beforeAll hooks to unmount tests

--- tests/002-unmount.spec.ts
+++ tests/002-unmount.spec.ts
@@ -1,5 +1,11 @@
 import { test, expect } from "@playwright/test";
 
+import checkForKoreroBuild from "./utils/checkForKoreroBuild";
+import setupAndTeardownMockServer from "./utils/setupAndTeardownMockServer";
+
+checkForKoreroBuild(test);
+setupAndTeardownMockServer(test);
+
 test("It should remove the container div on calling of unmount", async ({
 	page,
 }) => {
