75acf8d4d5e3be14e63a76f7a26daa08093cd4fd
Devesh Kumar <devesh2027@gmail.com>
2023-02-25 15:07:56 +0530

    Added finder package to get more specific selector for element targetting

--- src/utils/getAllIdentifyingAttrsForElement.ts
+++ src/utils/getAllIdentifyingAttrsForElement.ts
@@ -1,20 +1,28 @@
+// Some reading material:
+// https://davidfurlong.me/creating-unique-stable-selectors-dom-elements
+// https://chromium.googlesource.com/chromium/blink/+/master/Source/devtools/front_end/components/DOMPresentationUtils.js
+// Long story short: There is no reliable way to generate a unique selector given the plethora of DOM manipulating libraries like React.
+
+import { finder as elementUniqueSelectorGenerator } from "@medv/finder";
+
 export const getElementUniqueSelector = (element: HTMLElement | Element) => {
-	const path: string[] = [];
-	let parent: ParentNode | null;
+	// // Old code generated a body > div:nth-child(2) > div:nth-child(4) > div:nth-child(1) like selector.
+	// const path: string[] = [];
+	// let parent: ParentNode | null;
+	// while ((parent = element.parentNode)) {
+	// 	const children = Array.from(parent.children);
+	// 	const elementIndex = children.indexOf(element);
+	// 	path.unshift(
+	// 		`${element.tagName}${
+	// 			elementIndex ? `:nth-child(${elementIndex + 1})` : ""
+	// 		}`
+	// 	);
+	// 	element = parent as HTMLElement;
+	// }
+	// const selector = path.join(" > ").toLowerCase();
+	// return selector;
 
-	while ((parent = element.parentNode)) {
-		const children = Array.from(parent.children);
-		const elementIndex = children.indexOf(element);
-		path.unshift(
-			`${element.tagName}${
-				elementIndex ? `:nth-child(${elementIndex + 1})` : ""
-			}`
-		);
-		element = parent as HTMLElement;
-	}
-
-	const selector = path.join(" > ").toLowerCase();
-	return selector;
+	return elementUniqueSelectorGenerator(element);
 };
 
 const getAllIdentifyingAttributesForElement = (element: HTMLElement) => {
