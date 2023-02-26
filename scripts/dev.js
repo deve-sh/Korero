4e4487a7d8cde24df7824bb858ab1c834dfff2e5
Devesh Kumar <devesh2027@gmail.com>
2023-02-26 22:55:48 +0530

    Removed live server from dev script and turned it into a simple real-time script bundler

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,41 +1,5 @@
-const liveServer = require("live-server");
 const { execSync, exec } = require("child_process");
 const chokidar = require("chokidar");
-const { mkdirSync, writeFileSync } = require("fs");
-const open = require("open");
-
-const firebaseCredentials =
-	process.env.KORERO_FIREBASE_CREDENTIALS || process.argv[2];
-const config = process.env.KORERO_CONFIG || process.argv[3];
-
-if (!firebaseCredentials)
-	throw new Error(
-		"Korero Dev: Please add firebase credentials as a command line argument or FIREBASE_CREDENTIALS environment variable."
-	);
-
-const HTMLFileTemplate = `
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<title>Korero - Try it out</title>
-	</head>
-	<body>
-		<script type="text/javascript" src="../dist/umd.js"></script>
-		<script defer>
-			const k = new korero({
-				firebaseCredentials: ${firebaseCredentials},
-				options: ${
-					config ||
-					JSON.stringify({
-						allowedSignInMethods: ["google", "github"],
-					})
-				},
-			});
-			k.initialize();
-		</script>
-	</body>
-</html>
-`;
 
 let lastBuildEventTriggeredAt = null;
 chokidar.watch("./src").on("change", (path) => {
@@ -55,39 +19,5 @@ chokidar.watch("./src").on("change", (path) => {
 	}
 });
 
-// Create a build and start the live server.
-
-// Write index.html file.
-try {
-	mkdirSync("./dev");
-} catch {}
-
-writeFileSync("./dev/index.html", HTMLFileTemplate);
-
-console.log("Creating initial build for dev server...");
+console.log("Creating initial build");
 execSync("npm run build:dev", { stdio: "inherit" }); // Initial build
-const liveServerParams = {
-	port: 5500,
-	host: "localhost",
-	root: "./",
-	open: false,
-	ignore: "node_modules,src",
-	file: "index.html",
-	wait: 1000,
-	logLevel: 2,
-};
-liveServer.start(liveServerParams);
-const liveServerURL = "http://localhost:5500/dev/index.html";
-console.log("Live Server started at: " + liveServerURL);
-open(liveServerURL);
-
-const exitHandler = () => {
-	try {
-		liveServer.shutdown();
-	} catch {}
-};
-process.once("exit", exitHandler);
-process.once("SIGINT", exitHandler);
-process.once("SIGUSR1", exitHandler);
-process.once("SIGUSR2", exitHandler);
-process.once("uncaughtException", exitHandler);
